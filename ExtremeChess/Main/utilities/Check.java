540912c7674d78822555a3e17159d82f82711a80
Michael <mbench@student.neumont.edu>
2015-08-25 19:52:33 -0600

    simplify

--- ExtremeChess/Main/utilities/Check.java
+++ ExtremeChess/Main/utilities/Check.java
@@ -6,12 +6,12 @@ import gameobjects.BoardTile;
 public class Check {
 
 	public void check(BoardTile[][] b,BoardTile t,int x ,int y, boolean cap,MoveController c){
+		
 		boolean occupado = b[x][y].isOccupied();
 		String checkAgainst = b[x][y].pieceType;
 		String checkColor = b[x][y].pieceColor;
 		int toX = t.xPos - b[x][y].xPos;
 		int toY = t.yPos - b[x][y].yPos;
-
 		int validMovePos = 1;
 		int validMoveNeg = -1;
 
@@ -48,7 +48,7 @@ public class Check {
 				c.darkInCheck = true;
 			}
 		}
-		else if(occupado && checkAgainst.equalsIgnoreCase("q") && !b[x][y].pieceColor.equalsIgnoreCase(t.pieceColor)&&b[x][y].yPos - t.yPos == 0){
+		else if(occupado && checkAgainst.equalsIgnoreCase("q") && !checkColor.equalsIgnoreCase(t.pieceColor) && toY == 0){
 			if(t.pieceColor.equalsIgnoreCase("l")){
 				c.lightInCheck = true;
 				System.out.println("Light in check");
@@ -58,7 +58,7 @@ public class Check {
 				c.darkInCheck = true;
 			}
 		}
-		else if(b[x][y].isOccupied() && b[x][y].pieceType.equalsIgnoreCase("q") && !b[x][y].pieceColor.equalsIgnoreCase(t.pieceColor)&&Math.abs(b[x][y].yPos - t.yPos) == Math.abs(b[x][y].xPos - t.xPos) ){
+		else if(occupado && checkAgainst.equalsIgnoreCase("q") && !checkColor.equalsIgnoreCase(t.pieceColor)&&Math.abs(toY) == Math.abs(toX)){
 			if(t.pieceColor.equalsIgnoreCase("l")){
 				c.lightInCheck = true;
 				System.out.println("Light in check");
@@ -68,7 +68,7 @@ public class Check {
 				c.darkInCheck = true;
 			}
 		}
-		else if(b[x][y].isOccupied() && b[x][y].pieceType.equalsIgnoreCase("b") && !b[x][y].pieceColor.equalsIgnoreCase(t.pieceColor)&&Math.abs(b[x][y].yPos - t.yPos) == Math.abs(b[x][y].xPos - t.xPos) ){
+		else if(occupado && checkAgainst.equalsIgnoreCase("b") && !checkColor.equalsIgnoreCase(t.pieceColor)&&Math.abs(toY) == Math.abs(toX) ){
 			if(t.pieceColor.equalsIgnoreCase("l")){
 				c.lightInCheck = true;
 				System.out.println("Light in check");
@@ -78,19 +78,7 @@ public class Check {
 				c.darkInCheck = true;
 			}
 		}
-		else if(b[x][y].isOccupied() && b[x][y].pieceType.equalsIgnoreCase("n") && !b[x][y].pieceColor.equalsIgnoreCase(t.pieceColor) && toX == 2 && toY <=1 && toY >= -1  && toY != 0  ){
-			if(t.pieceColor.equalsIgnoreCase("l")){
-				c.lightInCheck = true;
-				System.out.println("Light in check");
-			}
-			else if(t.pieceColor.equalsIgnoreCase("d")){
-				System.out.println("Dark in check");
-				c.darkInCheck = true;
-			}
-
-
-		}
-		else if(b[x][y].isOccupied() && b[x][y].pieceType.equalsIgnoreCase("n") && !b[x][y].pieceColor.equalsIgnoreCase(t.pieceColor) && toX == -2 && toY <=1 && toY >= -1 && toY != 0  ){
+		else if(occupado && checkAgainst.equalsIgnoreCase("n") && !checkColor.equalsIgnoreCase(t.pieceColor) && toX == 2 && toY <=1 && toY >= -1  && toY != 0  ){
 			if(t.pieceColor.equalsIgnoreCase("l")){
 				c.lightInCheck = true;
 				System.out.println("Light in check");
@@ -100,8 +88,9 @@ public class Check {
 				c.darkInCheck = true;
 			}
 
+
 		}
-		else if(b[x][y].isOccupied() && b[x][y].pieceType.equalsIgnoreCase("n") && !b[x][y].pieceColor.equalsIgnoreCase(t.pieceColor) && toY == 2 &&  toX <=1 && toX >= -1 && toX != 0  ){
+		else if(occupado && checkAgainst.equalsIgnoreCase("n") && !checkColor.equalsIgnoreCase(t.pieceColor) && toX == -2 && toY <=1 && toY >= -1 && toY != 0  ){
 			if(t.pieceColor.equalsIgnoreCase("l")){
 				c.lightInCheck = true;
 				System.out.println("Light in check");
@@ -112,7 +101,18 @@ public class Check {
 			}
 
 		}
-		else if(b[x][y].isOccupied() && b[x][y].pieceType.equalsIgnoreCase("n") && !b[x][y].pieceColor.equalsIgnoreCase(t.pieceColor) && toY == 2 &&toY == -2 && toX <=1 && toX >= -1 && toX != 0   ){
+		else if(occupado && checkAgainst.equalsIgnoreCase("n") && !checkColor.equalsIgnoreCase(t.pieceColor) && toY == 2 &&  toX <=1 && toX >= -1 && toX != 0  ){
+			if(t.pieceColor.equalsIgnoreCase("l")){
+				c.lightInCheck = true;
+				System.out.println("Light in check");
+			}
+			else if(t.pieceColor.equalsIgnoreCase("d")){
+				System.out.println("Dark in check");
+				c.darkInCheck = true;
+			}
+
+		}
+		else if(occupado && checkAgainst.equalsIgnoreCase("n") && !checkColor.equalsIgnoreCase(t.pieceColor) && toY == 2 &&toY == -2 && toX <=1 && toX >= -1 && toX != 0 ){
 			if(t.pieceColor.equalsIgnoreCase("l")){
 				c.lightInCheck = true;
 				System.out.println("Light in check");
